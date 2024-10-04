ef994af9f808a019c521abec6bce0c38df16f17a
redxfy <150579399+redxfy@users.noreply.github.com>
2024-10-04 13:25:35 +0530

    Update .ml

--- .ml
+++ .ml
@@ -1,46 +1,25 @@
-open Js_of_ocaml
-
-(* Define types for the game elements *)
-type position = { x: float; y: float }
-type bunny = { id: string; mutable pos: position; mutable sad: bool }
-type player = { id: string; mutable pos: position }
-
-(* Initialize game elements *)
-let init () =
-  let player = { id = "bear"; pos = { x = 0.0; y = 0.0 } } in
-  let bunnies = [] in
-  (* Setup DOM elements, event listeners, etc. *)
-  let button = Dom_html.getElementById "reload-button" in
-  button##.onclick := Dom_html.handler (fun _ ->
-    Dom_html.window##reload;
-    Js._true
-  );
-
-  (* Add bunnies to the game *)
-  let add_bunny id =
-    let bunny = { id = id; pos = { x = Random.float 400.; y = Random.float 400. }; sad = true } in
-    (* More logic to handle the bunny *)
-    bunny
+let random_string length =
+  let chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789" in
+  let n = String.length chars in
+  let rec aux acc count =
+    if count = 0 then acc
+    else
+      let rand_char = chars.[Random.int n] in
+      aux (acc ^ String.make 1 rand_char) (count - 1)
   in
+  aux "" length
 
-  (* Example of adding multiple bunnies *)
-  let _ = List.init 5 (fun i -> add_bunny (Printf.sprintf "bunny-%d" (i + 1))) in
-
-  (* Main game loop would go here *)
-
-  (* Event handling example *)
-  let handle_click _ =
-    let x = Random.float 400. in
-    let y = Random.float 400. in
-    (* Update player position or other game state *)
-    player.pos <- { x; y };
-    Js._true
-  in
-
-  Dom_html.window##.onclick := Dom_html.handler handle_click;
-
-  (* More game initialization and logic *)
+let create_file_with_random_name extension =
+  let random_name = random_string 8 in
+  let filename = random_name ^ extension in
+  let oc = open_out filename in
+  Printf.fprintf oc "/* This is a generated file: %s */\n" filename;
+  close_out oc;
+  Printf.printf "Created file: %s\n" filename
 
 let () =
-  Dom_html.document##.readyState := Js.string "complete";
-  init ()
+  Random.self_init ();
+  create_file_with_random_name ".css";
+  create_file_with_random_name ".html";
+  create_file_with_random_name ".js";
+  
